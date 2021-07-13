63d9d175873c047223c5b046593a2c8b67996e18
Ilya <ilyapavlushchik221@gmail.com>
2021-07-13 20:05:57 +0300

    Урок 21
    -Сделал замену фоторгафий
    -Ввод цифр в калькулятор был уже реализован

--- js/script.js
+++ js/script.js
@@ -237,4 +237,23 @@ window.addEventListener('DOMContentLoaded', () => {
     startSlide();
   };
   slider();
+
+  //Команда
+  const ourTeam = () => {
+    const command = document.querySelector('.command');
+
+    command.addEventListener('mouseover', (e) => {
+      if (e.target.matches('.command__photo')) {
+        e.target.dataset.temp = e.target.src;
+        e.target.src = e.target.dataset.img;
+      }
+    });
+    command.addEventListener('mouseout', (e) => {
+      if (e.target.matches('.command__photo')) {
+        e.target.src = e.target.dataset.temp;
+        e.target.removeAttribute('data-temp');
+      }
+    });
+  };
+  ourTeam();
 });
