3f6a53d8427a78196d20828a09060e5e87cd2b09
Ilya <ilyapavlushchik221@gmail.com>
2021-07-14 17:23:38 +0300

    Урок 22
    -Сделал калькулятор

--- js/script.js
+++ js/script.js
@@ -256,4 +256,41 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   };
   ourTeam();
+
+  //Калькулятор
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+      calcType = document.querySelector('.calc-type'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcCount = document.querySelector('.calc-count'),
+      calcDay = document.querySelector('.calc-day'),
+      totalValue = document.getElementById('total');
+
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) countValue += (calcCount.value - 1) / 10;
+
+      if (calcDay.value && calcDay.value < 5) dayValue *= 2;
+      else if (calcDay.value && calcDay.value < 10) dayValue *= 1.5;
+
+      if (typeValue && squareValue)
+        total = price * squareValue * typeValue * countValue * dayValue;
+
+      totalValue.textContent = Math.floor(total);
+    };
+
+    calcBlock.addEventListener('input', (e) => {
+      const target = e.target;
+
+      if (target.matches('select, input')) {
+        countSum();
+      }
+    });
+  };
+  calc(100);
 });
